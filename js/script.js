bd4ba389e4b2dfd21111d6853972a45ff158c069
jake-innovates <jake.innovates@gmail.com>
2024-05-31 11:19:56 -0400

    update contact form

--- js/script.js
+++ js/script.js
@@ -52,3 +52,32 @@ window.onscroll = () => {
     }
 }
 
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const subject = document.getElementById("subject");
+const mess = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
+
+
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "jake.innovates@gmail.com",
+        Password: "D99338322D987A0BEAA19E1CAE6AD659B5B1",
+        To: 'jake.innovates@gmail.com',
+        From: "jake.innovates@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    })
+    .then(
+        message => alert(message)
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    sendEmail();
+});
